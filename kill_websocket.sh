5a977242bd9ac86abae0a33791db588669e4a95b
wwdd23 <woody@siqitao.com>
2020-07-01 17:04:06 +0800

    *update) 更新kill bash

--- kill_websocket.sh
+++ kill_websocket.sh
@@ -6,4 +6,8 @@
 # Created Time: 2020-06-29 03:52:07
 ###################
 
+
+echo "show pid:"
+ps aux | grep -ie python | grep -E 'okex|huobi|binance|bitmex' 
+
 ps aux | grep -ie python | grep -E 'okex|huobi|binance|bitmex' |awk '{print  $2}' |xargs kill -9

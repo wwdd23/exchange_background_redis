a8fd7d8d342aefb1a1ca431d873d547c295238f0
wwdd23 <woody@siqitao.com>
2020-07-06 01:01:08 +0800

    *update) 更新ws restart 脚本

--- restart.sh
+++ restart.sh
@@ -9,9 +9,18 @@
 echo "show pid:"
 ps aux | grep -ie python | grep -E 'okex|huobi|binance|bitmex' 
 
+echo "git pull start..."
+
+git pull;
+
+
+echo "kill ws pid"
 ps aux | grep -ie python | grep -E 'okex|huobi|binance|bitmex' |awk '{print  $2}' |xargs kill -9
 
+echo "restart ws python script"
 python3 binance.py > /dev/null 2>&1 &
 python3 okex.py > /dev/null 2>&1 &
 python3 bitmex.py > /dev/null 2>&1 &
 python3 huobi.py > /dev/null 2>&1 &
+
+echo "restart ws end"
